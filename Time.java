17c5d4bd47f4a3ae57440f41d392e0a1754071b1
souma83 <52839164+souma83@users.noreply.github.com>
2019-12-17 01:05:21 +0100

    Add files via upload

--- Time.java
+++ Time.java
@@ -2,7 +2,8 @@
 public class Time {
 	public static void main(String[]args){
 		
-	int hour=00, min=46, s=38;
-		System.out.println("the time is " + hour +":"+ min+":"+s);
+	int hour=00, min=46, second=38 , minsecond=60,hoursecond=3600;
+		System.out.println("the time is " + hour +":"+ min+":"+second);
+		System.out.println("the second midnight " min*mins+hour*hoursecond);		
 }
 }
