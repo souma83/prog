b7a1f3818fb825cf629c86c609ca356a43ff1138
souma83 <52839164+souma83@users.noreply.github.com>
2019-12-19 21:56:10 +0100

    Add files via upload

--- Time.java
+++ Time.java
@@ -4,7 +4,7 @@ public class Time {
 		
 	int hour=21, min=12, second=40 , minsecond=60 ,hoursecond=3600 ,midnight=min * minsecond +hour*hoursecond+second, 
 	minds=(24-hour)*3600+(0-min)*60+(0-second);
-	double porc=(24*3600/100)*midnight;
+	double proc=(midnight*100)/(24*3600);
 		System.out.println("the time is " + hour +":"+ min+":"+second);
 		System.out.println("the second midnight " +midnight);
 		System.out.println("the second remaining " +minds);
