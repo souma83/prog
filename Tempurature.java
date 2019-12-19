5587d5bebaab9f2956b75fb985e551147545b4b2
souma83 <52839164+souma83@users.noreply.github.com>
2019-12-19 22:50:34 +0100

    Add files via upload

--- Tempurature.java
+++ Tempurature.java
@@ -3,6 +3,8 @@ public class Tempurature {
 	public static void main(String[]args){
 		
 		double c=13, f=c*9/5+32;
+		System.out.println("inter the temprature in celsieus ");
+
 			System.out.println("the temprature is fahrenheit " + f);
 
 	}
