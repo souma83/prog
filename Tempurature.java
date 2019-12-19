987dddb64004f961fdc657695963f8dc43781c16
souma83 <52839164+souma83@users.noreply.github.com>
2019-12-19 23:03:22 +0100

    Add files via upload

--- Tempurature.java
+++ Tempurature.java
@@ -1,11 +1,11 @@
+	import java.util.Scanner;
 
 public class Tempurature {
 	public static void main(String[]args){
-		Scanner in=new Scanner(system.in);
-		double c f=c*9/5+32;
+		Scanner in = new Scanner(System.In);
+		double c, f=c*9/5+32;
 		System.out.println("enter the temprature in celsius ");
-		import java.util.Scanner;
-		c=in.nextdouble();
+		c=in.nextDouble();
 
 			System.out.println("the temprature is fahrenheit " + f);
 
